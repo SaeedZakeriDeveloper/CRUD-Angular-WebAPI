816dc30c47a09414d7b923eae382e449fa675537
Saeed Zakeri <saeed.zakeri.dev@gmail.com>
2024-07-01 19:35:37 +0330

    Added file

---
+++ WebApi/WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class Filter
+    {
+        public int take { get; set; }
+        public int skip { get; set; }
+    }
+    public class EmployeeController : ApiController
+    {
+        private DBModel db = new DBModel();
+        [HttpPost]
+        public IHttpActionResult GetEmployeesWithFilter(Filter filter)
+        {
+            var res = db.Employees.ToList();
+            return Ok(new
+            {
+                data = res,
+                totalCount = res.Count
+            });
+        }
+
+        //// GET: api/Employee/5
+        //[ResponseType(typeof(Employee))]
+        //public IHttpActionResult GetEmployee(int id)
+        //{
+        //    Employee employee = db.Employees.Find(id);
+        //    if (employee == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(employee);
+        //}
+
+        //// PUT: api/Employee/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutEmployee(int id, Employee employee)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != employee.EmployeeID)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(employee).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!EmployeeExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //// POST: api/Employee
+        //[HttpPost]
+        //[ResponseType(typeof(Employee))]
+        //public IHttpActionResult PostEmployee(Employee employee)
+        //{
+        //    // قسمت کامنت شده قرار است با انگولار پیاده سازی شود
+        //    //if (!ModelState.IsValid)
+        //    //{
+        //    //    return BadRequest(ModelState);
+        //    //}
+
+        //    db.Employees.Add(employee);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = employee.EmployeeID }, employee);
+        //}
+
+        //// DELETE: api/Employee/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employees.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
